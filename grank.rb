f286e0dba218e514dd47cde09eb40828dacaf2ff
Matt Wolfe <mwolfe38@gmail.com>
2009-03-30 22:36:27 -0700

    updated

--- grank.rb
+++ grank.rb
@@ -3,16 +3,24 @@ require 'rubygems'
 require 'open-uri'
 require 'hpricot'
 
-search = URI.escape("slide out shelves")
+def print_usage()
+   puts "usage: " + $FILENAME + "\"search\" domain"
+end
+if (ARGV.length < 2) 
+  print_usage()
+  exit   
+end
+search = URI.escape(ARGV[0])
 search_for_domain = "wolfehomeservices.com"
 found = false
 page_num = 0
 max_pages = 20
 while found == false and page_num < max_pages
-   result_num = page_num * 10
+    page_num += 1
+   result_num = (page_num-1) * 10
    elements = Hpricot.parse(open("http://www.google.com/search?q=#{search}&start=#{result_num}&sa=N")).search("ol li.g h3.r a")
    elements.each do | el |
-     host = URI.parse(el.attributes['href']).host
+     host = URI.parse(el.attributes['href']).host rescue next 
      if (host.include?(search_for_domain) or search_for_domain.include?(host))
          found = true
          break
@@ -20,7 +28,8 @@ while found == false and page_num < max_pages
    end
    if (found)
      puts "Found on page #{page_num}"
+     exit
    end
-   page_num += 1
+  
 end
 puts "search #{search} was not found within #{max_pages}"
