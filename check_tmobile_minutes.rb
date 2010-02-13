4c466eaf2035b477833c91397fe04a9d405d6cdb
Matt Wolfe <mwolfe@mwolfe-desktop.(none)>
2010-02-13 02:13:57 -0800

    updated to use json

--- check_tmobile_minutes.rb
+++ check_tmobile_minutes.rb
@@ -10,13 +10,15 @@
 require 'rubygems'
 require 'mechanize'
 require 'logger'
+require 'json'
 
+LOGIN_URL='https://my.t-mobile.com/Login/MyTMobileLogin.aspx'
+USAGE_URL='https://my.t-mobile.com/Services/account/accountservice.svc/GetAccountUseData'
+CYCLE_URL='https://my.t-mobile.com/Billing/Services/BillingService.svc/GetAccountActivityData'
 def get_usage_page(agent, phone_number, password)
   FileUtils.rm(['login_page.html', 'after_submit.html', 'usage_page.html'], :force=> true)
-  agent.user_agent_alias = 'Linux Mozilla'
-  login_url = 'https://my.t-mobile.com/Login/MyTMobileLogin.aspx'
-  usage_url = 'https://my.t-mobile.com/PartnerServices.aspx?service=eBill&link=MonthlyUsage';
-  page = agent.get(login_url)
+  agent.user_agent_alias = 'Linux Mozilla' 
+  page = agent.get(LOGIN_URL)
   File.open('login_page.html', 'w') {|f| f.write(page.body)}
   form = page.form('Form1')
   if (!form || !form.has_field?('Login1:txtMSISDN'))
@@ -28,13 +30,12 @@ def get_usage_page(agent, phone_number, password)
   form['Login1:txtPassword'] = password
   form['__EVENTTARGET'] = 'Login1$btnLogin'  
   newpage=agent.submit(form)
-  File.open('after_submit.html', 'w') {|f| f.write(newpage.body)}
-  agent.get(usage_url)
+  File.open('after_submit.html', 'w') {|f| f.write(newpage.body)}  
+  agent.get_file(USAGE_URL)
 end
 
-def get_cycle_page(agent)
-   url = 'https://ebill.t-mobile.com/myTMobile/pages/templates/unbilledUsageHeadingAcctTemplate.jsp'
-   agent.get(url)
+def get_cycle_page(agent)   
+   agent.get_file(CYCLE_URL)
 end
 
 def usage
@@ -54,32 +55,26 @@ agent = WWW::Mechanize.new { |a|
 }
 
 
-usage_page = get_usage_page(agent, phone_number,password)
-#usage_page = Nokogiri::HTML(File.open('usage_page.html', "r"))
+usage_json = get_usage_page(agent, phone_number,password)
+usage = JSON.parse(usage_json)
+pp usage
 
-#File.open('usage_page.html', 'w') {|f| f.write(usage_page.body)}
-
+used = usage['UsedMins']
+total= usage['TotalMins']
 cycle_page = get_cycle_page(agent)
-#cycle_page = Nokogiri::HTML(File.open('cycle_page.html', "r"))
-range = cycle_page.search("dl.definitionlist3 dd")[1].content.split('-').map {|el| el.strip}
+cycle = JSON.parse(cycle_page)
+from = Date.strptime(cycle['BillCycleStartDate'], '%m/%d/%Y')
+to = Date.strptime(cycle['BillCycleEndDate'], '%m/%d/%Y')
+now = Date.today
 
-from = Date.strptime(range[0], '%m/%d/%y')
-to = Date.strptime(range[1], '%m/%d/%y')
 billing_period_message =  'Billing Period: ' + from.to_s + ' to: ' + to.to_s
 now = Date.today
 
-minutes = usage_page.search('/html/body/div[2]/div[2]/div/div[3]/div/div/div[2]/div/div[3]/div/h3/span/text()')
-usage_message = "Usage: Unknown"
-alert_message = "";
-if (minutes != nil && minutes[0] != nil && minutes[2] != nil)
-  used = minutes[1].to_s.delete(':').strip.to_i
-  total = minutes[2].to_s.delete('/').strip.to_i
-  usage_percent = ((used.to_f/total.to_f) * 100.0).round()
-  
-  diff_to_end =  to - now
-  diff_from_start = now - from
-  diff_total = to - from
-
+usage_percent = ((used.to_f/total.to_f) * 100.0).round()
+diff_to_end =  to - now
+diff_from_start = now - from
+diff_total = to - from
+alert_message = ""
   days_used_percent = ((diff_from_start.to_f / diff_total.to_f) * 100).round()
   
   usage_message = "Usage: #{used} of #{total} minutes"
@@ -90,7 +85,7 @@ if (minutes != nil && minutes[0] != nil && minutes[2] != nil)
     alert_message = "CAREFUL - Your minutes are almost up\n"
     level = "--warning"
   else
-    alert_message = "STOP USING YOUR PHONE, YOUR MINUTES ARE BEING BILLED at \\$0.45 per minute\n" +msg
+    alert_message = "STOP USING YOUR PHONE, YOUR MINUTES ARE BEING BILLED at \\$0.45 per minute\n" 
     level = "--error"
   end   
 
@@ -101,10 +96,7 @@ if (minutes != nil && minutes[0] != nil && minutes[2] != nil)
   else 
     usage_percent_message = "You are using your minutes at an acceptable rate."
   end
-else
-  alert_message = "Unable to get t-mobile phone usage data\n"
-  level = "--error"  
-end
+
  msg = alert_message + "\n" + usage_message + "\n" + billing_period_message + "\n" + usage_percent_message + "\n"
 puts msg
 `zenity #{level} --text="#{msg}"`
